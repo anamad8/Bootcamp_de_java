aa9d5a50d69ece932a8a2cb9c5de8cd597e65437
Anabel Amad <stellam_anabel@hotmail.com>
2024-05-06 10:58:03 -0300

    clase 6 con ejercicios terminados

--- bootcamp_test/src/bootcamp_test/clase_6/Clase_6.java
+++ bootcamp_test/src/bootcamp_test/clase_6/Clase_6.java
@@ -2,10 +2,46 @@ package bootcamp_test.clase_6;
 
 import java.util.Scanner;
 
+import clase_6_ejercicio_1.Ejercicio_1;
+import clase_6_ejercicio_2.Ejercicio_2;
+import clase_6_ejercicio_3.Ejercicio_3;
+
+
+
 public class Clase_6 {
 	public static void main(String[] args) {
+			
+			 Scanner scanner = new Scanner(System.in);
+		        
+		        System.out.println("Seleccione el ejercicio que desea ejecutar:");
+		        System.out.println("1. Ejercicio 1");
+		        System.out.println("2. Ejercicio 2");
+		        System.out.println("1. Ejercicio 3");
+		        System.out.print("Ingrese el número del ejercicio: ");
+		        
+		        int opcion = scanner.nextInt();
+		        
+		        switch(opcion) {
+		            case 1:
+		                Ejercicio_1.main(args);
+		                break;
+		            case 2:
+		                Ejercicio_2.main(args);
+		                break;
+		            case 3:
+		                Ejercicio_3.main(args);
+		                break;
+		            default:
+		                System.out.println("Opción no válida");
+		        }
+		        
+		        scanner.close();
+		
+
+		
+		// Ejercico en la clase
 		// Cargar 3 num. y mostrar el menor y el mayor
-		Scanner teclado = new Scanner(System.in);
+		/*Scanner teclado = new Scanner(System.in);
 		System.out.println("Carge el primer numero:");
 		int numero1 = teclado.nextInt();
 		System.out.println("Carge el segundo numero:");
@@ -24,5 +60,6 @@ public class Clase_6 {
 		if(numero2 > numero1 && numero2 > numero3) {
 			System.out.println("El numero mas grande es: " + numero2);
 		}
+		*/
 	}
 }
--- bootcamp_test/src/clase_6_ejercicio_2/Ejercicio_2.java
+++ bootcamp_test/src/clase_6_ejercicio_2/Ejercicio_2.java
@@ -1,6 +1,9 @@
 package clase_6_ejercicio_2;
 
+import java.util.Scanner;
+
 public class Ejercicio_2 {
+	public static void main(String[] args) {
 	/*
 	 	Armar un buscador de twitter.com. Es  bastante similar al anterior.
 	 	Primero pidele al usuario que ingrese el nombre de una celebridad (de una sola palabra), (Ej: Messi).
@@ -8,4 +11,14 @@ public class Ejercicio_2 {
 	 	"https://twitter.com/search/" + palabra
 	 	Entosnces cuiando hagas click ene le link uqe aparesca en la consola, te abrira esa ubicacion.
 	 */
+	
+		Scanner cargaDatos = new Scanner(System.in);
+	
+	    System.out.println("Ingresa el nombre de una celebridad (Ej: Messi): ");
+	    String nombre = cargaDatos.nextLine(); 
+	
+	    String url = "https://twitter.com/search/" + nombre;
+	    System.out.println("URL generada: " + url);
+	    cargaDatos.close();
+	}
 }
